2f099159872db925b5a307bf68283f8d5eb2217c
Darioush Jalali <darioush.jalali@avalabs.org>
2025-01-08 11:18:00 -0800

    fix test

--- nomt/node_test.go
+++ nomt/node_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestChunkIndexAsInt(t *testing.T) {
 	chunkIdx := ChunkIndex([4]byte{1, 0, 1, 0})
-	require.Equal(t, int(0x01000100), chunkIdx.AsInt())
+	require.Equal(t, uint32(0x01000100), chunkIdx.AsInt())
 }
 
 func TestLeafNodeChunks(t *testing.T) {
